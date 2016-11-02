aa295e430c73dcf57a4116a47c1071365130628f
SERGIO DANIEL FERNANDEZ ATOCHERO <sergiodaniel.fernandez.contractor@bbva.com>
2016-11-02 13:37:38 +0100

    Changes name module

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sergio D Fdez',
   author_email = 'sergiodfdez@gmail.com',
   url = 'https://github.com/Sergiodfdez/python-module-loader', # use the URL to the github repo
-  download_url = 'https://github.com/Sergiodfdez/python-module-loader/tarball/0.1.6', # I'll explain this in a second
+  download_url = 'https://github.com/Sergiodfdez/python-moduleloader/tarball/0.1.6', # I'll explain this in a second
   keywords = ['module', 'load', 'external', 'directory'], # arbitrary keywords
   classifiers = []
 )
